d171ce7967cb8ef1d47e87c95e51f6d54848c75e
MatheusOsterno <matheusosternodev@gmail.com>
2022-09-06 23:55:37 -0300

    m

---
+++ src/components/Banner.vue
@@ -0,0 +1,28 @@
+<template>
+   <div id="main-banner">
+    <h1> Make Tour Pizza</h1>
+   </div>
+</template>
+
+<script>
+
+export default{
+    name: 'banner'
+}
+   
+</script>
+
+<style scoped>
+
+    #main-banner {
+        background-image: url('../img/wallpaperpizza.jpg');
+        background-position: 0 250;
+        background-size: cover;
+        height: 500px;
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+
+    }
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,23 @@
 <template>
+  <banner />
   <div class="main-container">
-    <h1> Home </h1>
+    <h1> Monte sua pizza: </h1>
+    <p>formulário</p>
   </div>
 </template>
 
 <script>
+import banner from '../components/Banner.vue'
+
+export default {
+  name: "Home",
+  components: {
+    banner
+  }
+}
 
 </script>
+
+<style scoped>
+
+</style>
