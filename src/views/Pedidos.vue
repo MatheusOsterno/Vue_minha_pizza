20f94b175c56298fae420261a9bf3b0c629ffd72
MatheusOsterno <matheusosternodev@gmail.com>
2022-09-14 22:27:26 -0300

    Dashboard

--- src/views/Pedidos.vue
+++ src/views/Pedidos.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="main-container">
-    <h1>Pedidos</h1>
+    <dashboard />
   </div>
 </template>
+
+<script>
+import Dashboard from '@/components/Dashboard.vue'
+export default {
+  components: { Dashboard },
+  name: "Pedidos"
+}
+</script>
+
