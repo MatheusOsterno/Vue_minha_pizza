8942dd5caa86c0b0c54259c60b75f58959e2fceb
MatheusOsterno <matheusosternodev@gmail.com>
2022-09-15 01:16:57 -0300

    Dashboard2

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -2,7 +2,7 @@
 <template>
     <div id="pizza-table" v-if="pizzas">
       <div>
-        <div id="burger-table-heading">
+        <div id="pizza-table-heading">
           <div class="order-id">#:</div>
           <div>Cliente:</div>
           <div>Tamanho:</div>
@@ -11,24 +11,24 @@
           <div>Ações:</div>
         </div>
       </div>
-      <div id="burger-table-rows">
-        <div class="burger-table-row" v-for="burger in burgers" :key="burger.id">
-          <div class="order-number">{{ burger.id }}</div>
+      <div id="pizza-table-rows">
+        <div class="pizza-table-row" v-for="pizza in pizzas" :key="pizza.id">
+          <div class="order-number">{{ pizza.id }}</div>
           <div>{{ pizza.nome }}</div>
           <div>{{ pizza.tamanho }}</div>
-          <div>{{ burger.massa }}</div>
+          <div>{{ pizza.massa }}</div>
           <div>
-            <ul v-for="(opcional, index) in burger.opcionais" :key="index">
+            <ul v-for="(opcional, index) in pizza.opcionais" :key="index">
               <li>{{ opcional }}</li>
             </ul>
           </div>
           <div>
-            <select name="status" class="status" @change="updateBurger($event, burger.id)">
-              <option :value="s.tipo" v-for="s in status" :key="s.id" :selected="burger.status == s.tipo">
+            <select name="status" class="status" @change="updatePizza($event, pizza.id)">
+              <option :value="s.tipo" v-for="s in status" :key="s.id" :selected="pizza.status == s.tipo">
                 {{ s.tipo }}
               </option>
             </select>
-            <button class="delete-btn" @click="deleteBurger(burger.id)">Cancelar</button>
+            <button class="delete-btn" @click="deletePizza(pizza.id)">Cancelar</button>
           </div>
         </div>
       </div>
@@ -39,38 +39,80 @@
   </template>
   
   <script>
-  export default {
-    name: "Pedidos"
-  }
+    export default {
+      name: "Dashboard",
+      data() {
+        return {
+          pizzas: null,
+          pizza_id: null,
+          status: []
+        }
+      },
+      methods: {
+        async getPedidos() {
+          const req = await fetch('http://localhost:3000/pizzas')
+          const data = await req.json()
+          this.pizzas = data
+          // Resgata os status de pedidos
+          this.getStatus()
+        },
+        async getStatus() {
+          const req = await fetch('http://localhost:3000/status')
+          const data = await req.json()
+          this.status = data
+        },
+        async deleteBurger(id) {
+          const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+            method: "DELETE"
+          });
+          const res = await req.json()
+          this.getPedidos()
+        },
+        async updateBurger(event, id) {
+          const option = event.target.value;
+          const dataJson = JSON.stringify({status: option});
+          const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+            method: "PATCH",
+            headers: { "Content-Type" : "application/json" },
+            body: dataJson
+          });
+          const res = await req.json()
+          console.log(res)
+        }
+      },
+      mounted () {
+      this.getPedidos()
+      }
+    }
   </script>
   
   <style scoped>
-    #burger-table {
+    #pizza-table {
       max-width: 1200px;
       margin: 0 auto;
     }
-    #burger-table-heading,
-    #burger-table-rows,
-    .burger-table-row {
+    #pizza-table-heading,
+    #pizza-table-rows,
+    .pizza-table-row {
       display: flex;
       flex-wrap: wrap;
     }
-    #burger-table-heading {
+    #pizza-table-heading {
       font-weight: bold;
       padding: 12px;
       border-bottom: 3px solid #333;
     }
-    .burger-table-row {
+    .pizza-table-row {
       width: 100%;
       padding: 12px;
       border-bottom: 1px solid #CCC;
     }
-    #burger-table-heading div,
-    .burger-table-row div {
+    #pizza-table-heading div,
+    .pizza-table-row div {
       width: 19%;
     }
-    #burger-table-heading .order-id,
-    .burger-table-row .order-number {
+    #pizza-table-heading .order-id,
+    .pizza-table-row .order-number {
       width: 5%;
     }
     select {
